894555c261fcfcc625322d3b3e671ea9bf8d3b84
Adam A <adam@hack.bg>
2022-07-18 11:19:10 +0300

    feat(izomorf@3.0.0): new layout of output package

--- izomorf/izomorf.cjs
+++ izomorf/izomorf.cjs
@@ -7,28 +7,15 @@ const process = require('process')
 const concurrently = require('concurrently')
 const fetch = require('node-fetch')
 module.exports = module.exports.default = izomorf
-if (require.main === module) izomorf(process.cwd(), ...process.argv.slice(2))
 const TSC = process.env.TSC || 'tsc'
+if (require.main === module) izomorf(process.cwd(), ...process.argv.slice(2)).then(()=>process.exit(0))
 async function izomorf (cwd, dryWet, ...publishArgs) {
 
   const dry = dryWet !== 'wet'
 
-  if (dry && !publishArgs.includes('--dry-run')) {
-    publishArgs.unshift('--dry-run')
-  }
-
-  if (!dry) {
-    // Start with a dry run
-    execFileSync(
-      'pnpm', ['publish', '--dry-run'],
-      { cwd, stdio: 'inherit', env: process.env }
-    )
-  }
-
   // Read package.json
   const original     = readFileSync($('package.json'), 'utf8')
   const packageJson  = JSON.parse(original)
-  const isTypescript = (packageJson.main||'').endsWith('.ts')
 
   // Check if this version is already uploaded
   const name    = packageJson.name
@@ -42,36 +29,63 @@ async function izomorf (cwd, dryWet, ...publishArgs) {
     throw new Error(`izomorf: NPM returned ${response.statusCode}`)
   }
 
+  if (dry && !publishArgs.includes('--dry-run')) {
+    publishArgs.unshift('--dry-run')
+  }
+
+  if (!dry) {
+    // Start with a dry run
+    execFileSync(
+      'pnpm', ['publish', '--dry-run'],
+      { cwd, stdio: 'inherit', env: process.env }
+    )
+  }
+
   // Patch package.json
   try {
-
+    const isTypescript = (packageJson.main||'').endsWith('.ts')
     if (isTypescript) {
-
-      const result = await concurrently([
-        `${TSC} --outDir dist/esm --target es6 --module es6 --declaration --declarationDir dist/dts`,
-        `${TSC} --outDir dist/cjs --target es6 --module commonjs`
-      ]).result
-
-      // Compile TS -> JS
-      const source           = $(packageJson.main || 'index.ts')
-      const browserSource    = $(packageJson.browser || source)
+      const dtsOut = 'dist/dts'
+      const esmOut = 'dist/esm'
+      const cjsOut = 'dist/cjs'
       const replaceExtension = (x, a, b) => `${basename(x, a)}${b}`
-      const dtsBuild         = $('dist/dts', replaceExtension(source, '.ts', '.js'))
-      const esmBuild         = $('dist/esm', replaceExtension(source, '.ts', '.js'))
-      const cjsBuild         = $('dist/cjs', replaceExtension(source, '.ts', '.js'))
-
+      // Compile TS -> JS
+      const result = await concurrently([
+        `${TSC} --outDir ${esmOut} --target es2016 --module es6 --declaration --declarationDir ${dtsOut}`,
+        `${TSC} --outDir ${cjsOut} --target es6 --module commonjs`
+      ]).result
+      for (const file of readdirSync($(dtsOut))) {
+        if (file.endsWith('.d.ts')) {
+          copyFileSync($(dtsOut, file), $(cwd, file))
+        }
+      }
+      for (const file of readdirSync($(esmOut))) {
+        if (file.endsWith('.js')) {
+          copyFileSync($(dtsOut, file), $(cwd, replaceExtension(file, '.js', '.esm.js')))
+        }
+      }
+      for (const file of readdirSync($(cjsOut))) {
+        if (file.endsWith('.js')) {
+          copyFileSync($(dtsOut, file), $(cwd, replaceExtension(file, '.js', '.cjs.js')))
+        }
+      }
+      const main        = $(packageJson.main    || 'index.ts')
+      const browserMain = $(packageJson.browser || 'index.browser.ts')
+      const dtsMain     = replaceExtension(main, '.ts', '.d.ts')
+      const esmMain     = replaceExtension(main, '.ts', '.esmjs')
+      const cjsMain     = replaceExtension(main, '.ts', '.esm.js')
       // Set main, types, and exports fields in package.json
       if (packageJson.type === 'module') {
         Object.assign(packageJson, {
-          main:    toRel(esmBuild),
-          types:   toRel(dtsBuild),
-          exports: { source: toRel(source), require: toRel(cjsBuild), default: toRel(esmBuild) },
+          main:    toRel(esmMain),
+          types:   toRel(dtsMain),
+          exports: { source: toRel(source), require: toRel(cjsMain), default: toRel(esmMain) },
         })
       } else {
         Object.assign(packageJson, {
-          main:    toRel(cjsBuild),
-          types:   toRel(dtsBuild),
-          exports: { source: toRel(source), import: toRel(esmBuild), default: toRel(cjsBuild) },
+          main:    toRel(cjsMain),
+          types:   toRel(dtsMain),
+          exports: { source: toRel(source), import: toRel(esmMain), default: toRel(cjsMain) },
         })
       }
 
