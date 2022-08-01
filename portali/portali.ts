2977a7ecc71c8c0e7692da40ca849890ff4b8159
Adam A <adam@hack.bg>
2022-08-01 17:02:25 +0300

    FIX(portali@1.2.3): allow undefined to be passed to get

--- portali/portali.ts
+++ portali/portali.ts
@@ -83,7 +83,7 @@ export class Endpoint {
   constructor (url: string) {
     this.url = new URL(url)
   }
-  get (pathname: string = '', params: Record<string, string> = {}): Promise<any> {
+  get (pathname: string = '', params: Record<string, string|undefined> = {}): Promise<any> {
     const url = Object.assign(new URL(this.url.toString()), { pathname })
     for (const [key, value] of Object.entries(params)) {
       url.searchParams.set(key, value)
