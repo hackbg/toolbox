1278fd95019709af3a4eb2a5196ad37d4dece6b5
Adam A <adam@hack.bg>
2022-08-11 03:38:41 +0300

    WIP: FEAT(izomorf@4.2): gotta do our own import rewriting :(

--- izomorf/izomorf.cjs
+++ izomorf/izomorf.cjs
@@ -1,35 +1,54 @@
 #!/usr/bin/env node
+// Bunch of Node-natives
 const { extname, resolve, basename, relative, join, isAbsolute } = require('path')
 const { existsSync, readFileSync, writeFileSync, readdirSync, copyFileSync, unlinkSync } = require('fs')
 const { execSync, execFileSync } = require('child_process')
 const { request } = require('https')
 const process = require('process')
 
+// To bail early if the package is already uploaded
+const fetch        = require('node-fetch')
+
+// To speed things up - runs ESM and CJS compilations in paralle
 const concurrently = require('concurrently')
-const fetch = require('node-fetch')
 
-const TSC    = process.env.TSC || 'tsc'
-const dtsOut = 'dist/dts'
-const esmOut = 'dist/esm'
-const cjsOut = 'dist/cjs'
-const distDtsExt = '.dist.d.ts'
-const distEsmExt = '.dist.mjs'
-const distCjsExt = '.dist.cjs'
-const distJsExt  = '.dist.js'
+// To fix import statements when compiling to ESM
+const recast       = require('recast')
 
+// To draw boxes around things
+const boxen        = require('boxen')
+
+// TypeScript compiler
+const TSC          = process.env.TSC || 'tsc'
+
+// Temporary output directories
+const dtsOut       = 'dist/dts'
+const esmOut       = 'dist/esm'
+const cjsOut       = 'dist/cjs'
+const distDirs     = [dtsOut, esmOut, cjsOut]
+
+// Output extensions
+const distDtsExt   = '.dist.d.ts'
+const distEsmExt   = '.dist.mjs'
+const distCjsExt   = '.dist.cjs'
+const distJsExt    = '.dist.js'
+const distExts     = [distDtsExt, distEsmExt, distCjsExt, distJsExt]
+
+// What it says on the tin:
 const replaceExtension = (x, a, b) => `${basename(x, a)}${b}`
 
+// Entry point:
 if (require.main === module) izomorf(process.cwd(), ...process.argv.slice(2))
   .then(()=>process.exit(0))
-  .catch(e=>{
-    console.error(e)
-    process.exit(1)
-  })
+  .catch(e=>{console.error(boxen(e)); process.exit(1)})
 
+// Export for programmatical reuse:
 module.exports = module.exports.default = izomorf
 
+// Main function:
 async function izomorf (cwd, command, ...publishArgs) {
 
+  // Dispatch command.
   switch (command) {
     case 'dry':   return await release(false)
     case 'fix':   return await release(false, true)
@@ -38,10 +57,15 @@ async function izomorf (cwd, command, ...publishArgs) {
     default:      return usage()
   }
 
+  /** Remove output directories and output files */
   async function clean () {
-    await concurrently([dtsOut, esmOut, cjsOut].map(out=>`rm -rf ${out}`))
+    await Promise.all([
+      concurrently(distDirs.map(out=>`rm -rf ${out}`)),
+      concurrently(distExts.map(ext=>`rm -rf *${ext}`))
+    ])
   }
 
+  /** Perform a release. */
   async function release (
     /** Whether to actually publish to NPM, or just go through the movements ("dry run")  */
     wet  = false,
@@ -52,10 +76,18 @@ async function izomorf (cwd, command, ...publishArgs) {
     // Read package.json
     const original    = readFileSync($('package.json'), 'utf8')
     const packageJson = JSON.parse(original)
-    console.log('Original package.json:', packageJson, '\n')
-    // Check if this version is already uploaded
+    // Check if this version is already released
     const name     = packageJson.name
     const version  = packageJson.version
+    // 1. Bail if Git tag already exists
+    const tag      = `npm/${name}/${version}`
+    try {
+      execFileSync('git', ['rev-parse'], tag, { cwd, stdio: 'inherit', env: process.env })
+      const msg = `Git tag ${tag} already exists. ` +
+        `Increment version in package.json or delete tag to proceed.`
+      throw new Error(msg)
+    } catch (e) {}
+    // 2. Bail if library already exists on NPM
     const url      = `https://registry.npmjs.org/${name}/${version}`
     const response = await fetch(url)
     if (response.status === 200) {
@@ -64,6 +96,8 @@ async function izomorf (cwd, command, ...publishArgs) {
     } else if (response.status !== 404) {
       throw new Error(`izomorf: NPM returned ${response.statusCode}`)
     }
+    // Print the original package.json
+    console.log('Original package.json:', packageJson, '\n')
     if (wet) {
       // Do a preliminary dry run
       execFileSync(
@@ -102,7 +136,6 @@ async function izomorf (cwd, command, ...publishArgs) {
           { cwd, stdio: 'inherit', env: process.env }
         )
       }
-      const tag = `npm/${packageJson.name}/${packageJson.version}`
       if (wet) {
         console.log('\nPublished:', tag)
         // Add Git tag
@@ -117,26 +150,21 @@ async function izomorf (cwd, command, ...publishArgs) {
       }
     } finally {
       if (keep) {
-        console.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        console.info("!!! Keeping the publish-ready package.json.              !!!")
-        console.info("!!! On-demand compilation of TS will not work with it!   !!!")
-        console.info("!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!!!")
-        console.info('!!! ("main" and "exports" in package.json now point to   !!!')
-        console.info('!!!  the compiled code instead of the original source)   !!!')
-        console.info("!!!                                                      !!!")
-        console.info('!!! You might want to not commit this modification, and  !!!')
-        console.info('!!! restore the original package.json before commit.     !!!')
-        console.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        console.info(boxen([
+          "Keeping the modified publish-ready `package.json` for your inspection.",
+          "Make sure you don't commit it! Use `git checkout package.json` to restore the real one.",
+          'On-demand compilation of TS might not work. ("main" and "exports" now point to',
+          'the compiled code instead of the original source).',
+        ].join('\n'), { padding: 1, margin: 1, borderStyle: 'arrows' }))
       } else {
-        console.info("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-        console.info("||| Restoring the original package.json.                 |||")
-        console.info("|||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|||")
-        console.info("||| Any build artifacts will remain in place!            |||")
-        console.info("||| Make sure that they are .gitignored, otherwise your  |||")
-        console.info("||| repo will get crufty.                                |||")
-        console.info("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
         // Restore original contents of package.json
         writeFileSync($('package.json'), original, 'utf8')
+        console.info(boxen([
+          "Restoring the original package.json. Build artifacts (`*.dist.js`, etc.)",
+          "will remain in place. Make sure you don't commit them! Add their extensions",
+          "to `.gitignore` if you haven't already, and invoke this tool in `clean` mode",
+          "to get rid of them.",
+        ].join('\n'), { padding: 1, margin: 1, borderStyle: 'arrows' }))
       }
     }
     return packageJson
