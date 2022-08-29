5a421c45b3700b4013e1f108c245ed3c6eb271a8
Adam A <adam@hack.bg>
2022-08-30 02:45:48 +0300

    FEAT+BREAK(konfizi): add getNumber, change getStr to getString and getBool to getBoolean

--- konfizi/konfizi.ts
+++ konfizi/konfizi.ts
@@ -1,5 +1,15 @@
 export type Env = Record<string, string|undefined>
 
+export class EnvConfigError extends Error {
+
+  static Required = class EnvConfigRequiredError extends EnvConfigError {
+    constructor (name: string, type: string) {
+      super(`The environment variable ${name} must be a ${type}`)
+    }
+  }
+
+}
+
 export class EnvConfig {
 
   constructor (
@@ -7,23 +17,44 @@ export class EnvConfig {
     readonly cwd: string = ''
   ) {}
 
-  getStr <T> (name: string, fallback?: ()=>T): string|T {
+  getNumber <T> (name: string, fallback?: ()=>T): number|T {
+    if (this.env.hasOwnProperty(name)) {
+      const value = (process.env[name]??'').trim()
+      if (value === '') {
+        if (fallback) {
+          return fallback()
+        } else {
+          throw new EnvConfigError.Required(name, 'number')
+        }
+      }
+      const number = Number(value)
+      if (isNaN(number)) throw new EnvConfigError.Required(name, 'number')
+      return number
+    } else if (fallback) {
+      return fallback()
+    } else {
+      throw new EnvConfigError.Required(name, 'number')
+    }
+  }
+
+  getString <T> (name: string, fallback?: ()=>T): string|T {
     if (this.env.hasOwnProperty(name)) {
       return String(process.env[name] as string)
     } else if (fallback) {
       return fallback()
     } else {
-      throw new Error(`The environment variable ${name} (string) is required.`)
+      throw new EnvConfigError.Required(name, 'string')
     }
   }
 
-  getBool <T> (name: string, fallback?: ()=>T): boolean|T {
+  getBoolean <T> (name: string, fallback?: ()=>T): boolean|T {
     if (this.env.hasOwnProperty(name)) {
-      let value = (process.env[name]??'').trim()
+      const value = (process.env[name]??'').trim()
       return !this.FALSE.includes(value)
     } else if (fallback) {
       return fallback()
     } else {
+      throw new EnvConfigError.Required(name, 'boolean')
       throw new Error(`The environment variable ${name} (boolean) is required.`)
     }
   }
