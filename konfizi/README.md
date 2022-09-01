2da3419053d4fec8665522e5f6abc72695ecab4f
Adam A <adam@hack.bg>
2022-09-01 23:29:30 +0300

    FEAT(konfizi@2.1): add Overridable

--- konfizi/README.md
+++ konfizi/README.md
@@ -7,28 +7,42 @@ Parses the values of "empty string", `false`, `no`, and `0` as negatives.
 Example usage with TypeScript:
 
 ```typescript
-export interface Config {
-  agentName:     string|null
-  agentAddress:  string|null
-  agentMnemonic: string|null
-  mainnetUrl:    string
-  testnetUrl:    string
-}
+import * as Konfizi from '@hackbg/konfizi'
 
-/** Get configuration from the environment. */
-function getConfig (
-  env: Record<string, string> = {}
-): Config {
-  const { Str, Bool } = getFromEnv(env)
-  return {
-    agentName:      Str('AGENT_NAME',     ()=>null),
-    agentAddress:   Str('AGENT_ADDRESS',  ()=>null),
-    agentMnemonic:  Str('AGENT_MNEMONIC', ()=>null),
-    isMainnet:     Bool('IS_MAINNET',     ()=>false),
-    mainnetUrl:     Str('MAINNET_URL',    ()=>'http://default.mainnet.url'),
-    testnetUrl:     Str('TESTNET_URL',    ()=>'http://default.testnet.url'),
+export class MyConfig extends Konfizi.EnvConfig {
+
+  /** This constructor must be copied into every subclass to support the passing of defaults.
+    * This is dumb, TODO fix in next version. */
+  constructor (
+    env:      Konfizi.Env       = {},
+    cwd:      string            = '',
+    defaults: Partial<MyConfig> = {}
+  ) {
+    super(env, cwd)
+    this.override(defaults)
   }
+
+  /* If you provide no default, when the env var is not defined and error will be thrown */
+  myStringField
+    = this.getString('MY_STRING_FIELD')
+
+  /* To provide a default value pass a function that returns the default */
+  myNumberField
+    = this.getNumber('MY_NUMBER_FIELD', () => 0)
+
+  /* To provide a default value pass a function that returns the default */
+  myBooleanField
+    = this.getBoolean('MY_BOOLEAN_FIELD', () =>
+      this.getBoolean('MY_FLAG',          () =>
+      this.getBoolean('MY_OTHER_FLAG',    () => {
+        if (Math.random() > 0.5) {
+          return true
+        } else {
+          return false
+        }
+      })))
+
 }
 
-const currentConfig = getConfig(process.env)
+const currentConfig = new MyConfig(process.env, process.cwd, /* {}: defaults */)
 ```
--- konfizi/konfizi.ts
+++ konfizi/konfizi.ts
@@ -1,21 +1,75 @@
-export type Env = Record<string, string|undefined>
-
-export class EnvConfigError extends Error {
-
-  static Required = class EnvConfigRequiredError extends EnvConfigError {
-    constructor (name: string, type: string) {
-      super(`The environment variable ${name} must be a ${type}`)
-    }
+/** A ***value object*** that allows its meaningful properties to be overridden.
+  * For the override to work, empty properties must be defined as:
+  *
+  *     class Named extends Overridable {
+  *         name?: Type = undefined
+  *     }
+  *
+  * (Otherwise Object.getOwnPropertyNames wouldn't see the property slots,
+  * and `new Named.where({ name: 'something' })` wouldn't update `name`.
+  * This is because of how TypeScript handles class properties;
+  * in raw JS, they seem to be defined as undefined by default?)
+  *
+  * Even in when not inheriting from `Overridable`, try to follow the pattern of
+  * ***immutable value objects*** which represent a piece of state in context
+  * and which, instead of mutating themselves, emit changed copies of themselves
+  * using the idioms:
+  *     this.where({ name: 'value' }) // internally
+  * or:
+  *     new Named(oldNamed, { name: 'value' }) // externally.
+  **/
+export class Overridable {
+  override (options: object = {}) {
+    override(false, this, options)
   }
-
+  /** Return copy of self with overridden properties. */
+  where (options: Record<string, unknown> = {}) {
+    return new (this.constructor as any)(this, options)
+  }
 }
 
-export class EnvConfig {
+/** Override only allowed properties. */
+export function override (
+  /** Whether to fail on unexpected properties. */
+  strict:    boolean,
+  /** The object being overridden. */
+  self:      object,
+  /** The object containing the overrides. */
+  overrides: object,
+  /** List of allowed properties (defaults to the defined properties on the object;
+    * that's why many fields explicitly default to `undefined` - otherwise TypeScript
+    * does not generate them, somewhat contrarily to native JS class behavior) */
+  allowed:   string[] = Object.getOwnPropertyNames(self),
+): Record<string, valof<typeof overrides>> {
+  const filtered: Record<string, valof<typeof overrides>> = {}
+  for (const [key, val] of Object.entries(overrides)) {
+    if (allowed.includes(key)) {
+      const current: typeof val = (self as any)[key]
+      if (strict && current && current !== val) {
+        throw new Error(`Tried to override pre-defined ${key}`)
+      }
+      (self as any)[key] = val
+    } else {
+      (filtered as any)[key] = val
+    }
+  }
+  return filtered
+}
+
+export type Env = Record<string, string|undefined>
+
+export class EnvConfig extends Overridable {
 
   constructor (
     readonly env: Env    = {},
-    readonly cwd: string = ''
-  ) {}
+    readonly cwd: string = '',
+    defaults: Partial<EnvConfig> = {}
+  ) {
+    super()
+    Object.defineProperty(this, 'env', { writable: true, enumerable: false })
+    Object.defineProperty(this, 'cwd', { writable: true, enumerable: false })
+    this.override(defaults)
+  }
 
   getNumber <T> (name: string, fallback?: ()=>T): number|T {
     if (this.env.hasOwnProperty(name)) {
@@ -50,7 +104,7 @@ export class EnvConfig {
   getBoolean <T> (name: string, fallback?: ()=>T): boolean|T {
     if (this.env.hasOwnProperty(name)) {
       const value = (process.env[name]??'').trim()
-      return !this.FALSE.includes(value)
+      return !EnvConfig.FALSE.includes(value)
     } else if (fallback) {
       return fallback()
     } else {
@@ -59,6 +113,16 @@ export class EnvConfig {
     }
   }
 
-  FALSE = [ '', 'false', 'no', '0' ]
+  static FALSE = [ '', 'false', 'no', '0' ]
+
+}
+
+export class EnvConfigError extends Error {
+
+  static Required = class EnvConfigRequiredError extends EnvConfigError {
+    constructor (name: string, type: string) {
+      super(`The environment variable ${name} must be a ${type}`)
+    }
+  }
 
 }
