435f9c0baffff47f77745cf3544e8dc7eed5c0a1
Adam A <adam@hack.bg>
2022-09-14 16:13:43 +0300

    FEAT(komandi): use relative paths in initial CLI status message

--- komandi/komandi.cli.cjs
+++ komandi/komandi.cli.cjs
@@ -5,13 +5,18 @@ let ganesha; try { ganesha = require.resolve('@hackbg/ganesha') } catch (e) {
   console.warn(e)
   console.warn('Could not find @hackbg/ganesha. TypeScript not available.')
 }
+const { relative } = require('path')
+const shortPathTo = abs => {
+  const rel = relative(process.cwd(), abs)
+  return (rel.startsWith('..')) ? abs : rel
+}
 const interpreter = process.argv[0]
 const transpiler  = ganesha
 const entrypoint  = process.argv[1]
 const argv        = process.argv.slice(2)
-console.info('  Interpreter:', interpreter)
-console.info('  Transpiler: ', transpiler)
-console.info('  Entrypoint: ', entrypoint)
+console.info('  Interpreter:', shortPathTo(interpreter))
+console.info('  Transpiler: ', shortPathTo(transpiler))
+console.info('  Entrypoint: ', shortPathTo(entrypoint))
 console.info('  Arguments:  ', argv)
 console.info()
 require('@hackbg/ganesha').main([interpreter, transpiler, entrypoint, ...argv])
