b1a18523e33c455d8983f9365befdf362472e2a4
Adam A <adam@hack.bg>
2022-10-11 17:50:40 +0300

    FEAT(komadi): debug flag in CLI

--- komandi/komandi.cli.cjs
+++ komandi/komandi.cli.cjs
@@ -14,9 +14,19 @@ const interpreter = process.argv[0]
 const transpiler  = ganesha
 const entrypoint  = process.argv[1]
 const argv        = process.argv.slice(2)
-console.info('  Interpreter:', shortPathTo(interpreter))
-console.info('  Transpiler: ', shortPathTo(transpiler))
-console.info('  Entrypoint: ', shortPathTo(entrypoint))
-console.info('  Arguments:  ', argv)
-console.info()
+if (process.env.Komandi_Debug) {
+  console.info('  Interpreter:', shortPathTo(interpreter))
+  if (transpiler) console.info('  Transpiler: ', shortPathTo(transpiler))
+  console.info('  Entrypoint: ', shortPathTo(entrypoint))
+  console.info('  Arguments:  ', argv)
+  console.info()
+  // Modification to process.env persists in child process
+  process.env.NODE_OPTIONS = "--inspect"
+  console.info('  Debug mode. Will run Node with --inspect.')
+  if (ganesha) {
+    process.env.Ganesha_NoSourceMap = "1"
+    console.info('  Runtime source maps disabled.')
+  }
+}
+// FIXME run with just Node if missing Ganesha
 require('@hackbg/ganesha').main([interpreter, transpiler, entrypoint, ...argv])
