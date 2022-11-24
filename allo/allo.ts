a041a777e7cc9314d08717f116053e0e92a8df82
Adam A <adam@hack.bg>
2022-11-24 22:35:21 +0200

    FIX(allo): switch argument order to allow callables without parent classes; fix types

--- allo/allo.ts
+++ allo/allo.ts
@@ -1,15 +1,17 @@
-type Class<T> = new (...args: any[]) => T
+type Class<A extends unknown[], T> = new (...args: A) => T
 
 /** Define a callable class. Instances of the generated class can be invoked as functions.
   * The body of the function is passed as second argument.
   * The function's `this` identifier is bound to the instance.
   * @returns a callable class extending `Base`. */
-export function defineCallable <T, U extends T, F extends Function> (
-  Base: Class<T>,
-  fn:   F
-): Class<T> & F {
+export function defineCallable <F extends Function, A extends unknown[], T> (
+  /** The function that will be invoked when calling the instance. */
+  fn:   F,
+  /** The base class to be made callable. */
+  Base: Class<A, T> = class {} as Class<A, T>,
+): Class<A, T & F> {
   return Object.defineProperty(class extends (Base as any) {
-    constructor (...args: any) {
+    constructor (...args: A) {
       super(...args)
       const self = this
       const objectPrototype = Object.getPrototypeOf(self)
@@ -23,5 +25,5 @@ export function defineCallable <T, U extends T, F extends Function> (
       Object.defineProperties(call, Object.getOwnPropertyDescriptors(self))
       return call as T
     }
-  }, 'name', { value: `${Base.name}Callable` }) as Class<T> & F
+  }, 'name', { value: `${Base.name}Callable` }) as Class<A, T & F>
 }
