246288396a67c9b106397c9bd9fe216a6af1e923
Adam A <adam@hack.bg>
2023-02-05 10:31:04 +0200

    refactor(dock): dokeres.ts -> dock.ts
    extrack mock implementations to dock-mock.ts

---
+++ dock/dock-mock.ts
@@ -0,0 +1,43 @@
+import type { DockerHandle } from './dock'
+
+/** A stub implementation of the Dockerode APIs used by @hackbg/dock. */
+export function mockDockerode (callback: Function = () => {}): DockerHandle {
+  return {
+    getImage () {
+      return { async inspect () { return } }
+    },
+    getContainer (options: any) {
+      return mockDockerodeContainer(callback)
+    },
+    async pull () {},
+    buildImage () {},
+    async createContainer (options: any) {
+      return mockDockerodeContainer(callback)
+    },
+    async run (...args: any) {
+      callback({run:args})
+      return [{Error:null,StatusCode:0},Symbol()]
+    },
+    modem: {
+      followProgress (stream: any, complete: Function, callback: any) { complete(null, null) }
+    }
+  }
+}
+
+export function mockDockerodeContainer (callback: Function = () => {}) {
+  return {
+    id: 'mockmockmock',
+    logs (options: any, cb: Function) {
+      cb(...(callback({ createContainer: options })||[null, mockStream()]))
+    },
+    async start   () {},
+    async attach  () { return {setEncoding(){},pipe(){}} },
+    async wait    () { return {Error:null,StatusCode:0}  },
+    async inspect () { return {Image:' ',Name:null,Args:null,Path:null,State:{Running:null}}}
+  }
+}
+
+export function mockStream () {
+  return { on: () => {} }
+}
+
