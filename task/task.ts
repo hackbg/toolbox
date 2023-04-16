71ac3478b37c3a16c6cc653ef624d9fe50900027
Adam Avramov <adam.b.avramov@gmail.com>
2023-04-16 04:22:49 +0200

    chore(task): remove duplicate Timed class

--- task/task.ts
+++ task/task.ts
@@ -138,35 +138,3 @@ export class Task<C, X> extends Lazy<X> {
   static get [Symbol.species]() { return Promise; }
 
 }
-
-/** A task timer. */
-export abstract class Timed<T, U> {
-  started?: number|false = false
-  ended?:   number|false = false
-  result?:  T
-  failed?:  U
-  get took (): string|undefined {
-    return (this.ended && this.started)
-      ? `${((this.ended - this.started)/1000).toFixed(3)}s`
-      : undefined
-  }
-  protected start (): number {
-    if (this.started) throw new Error('Already started')
-    return this.started = + new Date()
-  }
-  protected end (): number {
-    if (!this.started) throw new Error('Not started yet')
-    if (this.ended) throw new Error('Already ended')
-    return this.ended = + new Date()
-  }
-  protected succeed (result: T): number {
-    const end = this.end()
-    this.result = result
-    return end
-  }
-  protected fail (failed: U): number {
-    const end = this.end()
-    this.failed = failed
-    throw failed
-  }
-}
