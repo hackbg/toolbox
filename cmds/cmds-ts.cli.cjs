9947dd1de903b71aa6fc82bdba07940275445f37
Adam Avramov <adam.b.avramov@gmail.com>
2023-04-18 23:44:36 +0200

    fix(@hackbg/cmds@1.2.2)

--- cmds/cmds-ts.cli.cjs
+++ cmds/cmds-ts.cli.cjs
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 
-// If TS loading is already enabled
-if (process.env.CMDS_TS) {
-  // Instantiate the default export of the commands module
-  console.info('TypeScript support enabled.\n')
-  const entrypoint = require('path').resolve(process.cwd(), process.argv[2])
-  import(entrypoint).then(module=>{
-    const commands = new (module.default)()
-    commands.run(process.argv.slice(3))
-  })
-} else {
+// Check if ganesha (ts loader) is available
+let ganesha
+try {
+  ganesha = require.resolve('@hackbg/ganesha')
+} catch (e) {
+  console.warn('@hackbg/ganesha not installed - scripting in TypeScript not available.')
+}
+
+if (ganesha && !process.env.CMDS_TS) {
+  // If ganesha is available and not enabled restart through ganesha
   console.info('Enabling TypeScript support...')
   if (process.env.CMDS_DEBUG) enableDebugMode()
-  // Check if ganesha is available
-  let ganesha
-  try {
-    ganesha = require.resolve('@hackbg/ganesha')
-  } catch (e) {
-    console.warn('@hackbg/ganesha not installed - scripting in TypeScript not available.')
-  }
-  // If ganesha is available restart through ganesha
   if (ganesha) {
     process.env.CMDS_TS = 1
     const argv = [process.argv[0], ganesha, ...process.argv.slice(1)]
     require('@hackbg/ganesha').main(argv)
   }
+} else {
+  // Run the default export of the commands module
+  if (process.env.CMDS_TS) console.info('TypeScript support enabled.\n')
+  const entrypoint = require('path').resolve(process.cwd(), process.argv[2])
+  import(entrypoint).then(module=>{
+    const commands = new (module.default)()
+    commands.run(process.argv.slice(3))
+  })
 }
 
 function enableDebugMode () {
