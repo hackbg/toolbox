6be8de2c60c9f42087d459686f906406e05ae15f
Adam Avramov <adam@hack.bg>
2023-07-23 17:18:53 +0300

    feat(emit): class Emitter

--- emit/emit.mjs
+++ emit/emit.mjs
@@ -1,9 +1,31 @@
-/** Embeddable event emitter. */
-export default function createEmitter (
+/** Extensible event emitter */
+export default class Emitter extends EventTarget {
+  emit (type, data={}) {
+    this.dispatchEvent(Object.assign(new Event(type), data))
+    return this
+  }
+  on (type, handler) {
+    this.addEventListener(type, handler)
+    return this
+  }
+  off (type, handler) {
+    this.removeEventListener(type, handler)
+    return this
+  }
+  once (type, handler) {
+    const onceHandler = () => {
+      this.off(type, onceHandler)
+      return handler(args)
+    }
+    return this.on(type, onceHandler)
+  }
+}
+
+/** Standalone event emitter. */
+export function createEmitter (
   retval,
   target = new EventTarget(),
 ) {
-
   const emitter = {
     emit: (type, data={}) => {
       target.dispatchEvent(Object.assign(new Event(type), data))
@@ -25,9 +47,6 @@ export default function createEmitter (
       return retval
     }
   }
-
   retval ??= emitter
-
   return emitter
-
 }
