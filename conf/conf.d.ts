c9ece3decdf58e59f46cb73307e402bb4907c366
Adam Avramov <adam@hack.bg>
2023-08-07 04:58:17 +0300

    release(conf@3.1.2): re-add types

---
+++ conf/conf.d.ts
@@ -0,0 +1,33 @@
+import { overrideFiltered } from '@hackbg/over'
+
+export class ConfigError extends Error {
+  static Required: ConfigError
+}
+
+export class Config {
+  constructor (environment: Environment)
+  readonly environment: Environment
+  override (options: object)
+  getFlag <T extends boolean, U> (name: string, fallback?: ()=>T|U): T|U
+  getString <T extends string, U> (name: string, fallback?: ()=>T|U): T|U
+  getNumber <T extends number, U> (name: string, fallback?: ()=>T|U): T|U
+}
+
+export class Environment {
+  constructor (cwd: string, vars: typeof process.env, tag:string|undefined)
+  /** Current working directory */
+  readonly cwd:  string
+  /** Environment variables */
+  readonly vars: typeof process.env
+  /** Tag to identify the environment (e.g. timestamp) */
+  readonly tag?: string
+  get [Symbol.toStringTag](): string
+  getFlag <T extends boolean, U> (name: string, fallback?: ()=>T|U): T|U
+  getString <T extends string, U> (name: string, fallback?: ()=>T|U): T|U 
+  getNumber <T extends number, U> (name: string, fallback?: ()=>T|U): T|U 
+  /** Snapshot of the earliest known process environment
+    * (at the moment this module is evaluated) */
+  static initial: Environment
+  /** Which string values are counted as falsy by getFlag */
+  static FALSE: string[]
+}
