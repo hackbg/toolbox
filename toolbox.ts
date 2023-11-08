40d707557afc15a389621a3ededcf476119c50f2
Adam Avramov <adam@hack.bg>
2023-11-08 22:22:20 +0200

    chore: remove internal dependencies

--- toolbox.ts
+++ toolbox.ts
@@ -1,11 +1,11 @@
 import open from 'open'
 import onExit from 'signal-exit'
 
-export * from '@hackbg/4mat'
-export * from '@hackbg/logs'
-export * from '@hackbg/file'
-export * from '@hackbg/cmds'
-export * from '@hackbg/dock'
+//export * from '@hackbg/4mat'
+//export * from '@hackbg/logs'
+//export * from '@hackbg/file'
+//export * from '@hackbg/cmds'
+//export * from '@hackbg/dock'
 
 export {
   open,
