2ce0408d45f6f3a1de6380a70c7ec2d63091b051
Adam Avramov <adam@hack.bg>
2023-11-21 04:59:32 +0200

    chore(dome): add type for render('img')

--- dome/dome.d.ts
+++ dome/dome.d.ts
@@ -11,6 +11,7 @@ declare module "@hackbg/dome" {
   export function render (name: 'select',   ...args: any): HTMLSelectElement
   export function render (name: 'input',    ...args: any): HTMLInputElement
   export function render (name: 'textarea', ...args: any): HTMLTextAreaElement
+  export function render (name: 'img',      ...args: any): HTMLImageElement
 
   export function bind <H extends HTMLElement> (
     element: HTMLElement, events: Record<string, Function>
