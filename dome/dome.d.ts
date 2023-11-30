7467a241565669487195c3ab2471dd6a77e2ac96
Adam Avramov <adam@hack.bg>
2023-11-30 19:50:51 +0200

    fix(dome): types

--- dome/dome.d.ts
+++ dome/dome.d.ts
@@ -6,12 +6,6 @@ declare module "@hackbg/dome" {
   export function frag (...nodes: Node[]): DocumentFragment
 
   export function render <H extends HTMLElement> (name: string, ...args: any): H
-  export function render (name: 'a',        ...args: any): HTMLAnchorElement
-  export function render (name: 'div',      ...args: any): HTMLDivElement
-  export function render (name: 'select',   ...args: any): HTMLSelectElement
-  export function render (name: 'input',    ...args: any): HTMLInputElement
-  export function render (name: 'textarea', ...args: any): HTMLTextAreaElement
-  export function render (name: 'img',      ...args: any): HTMLImageElement
 
   export function bind <H extends HTMLElement> (
     element: HTMLElement, events: Record<string, Function>
@@ -19,5 +13,6 @@ declare module "@hackbg/dome" {
 
   export function replace <H extends HTMLElement> (node: Element, newnode: H): H
 
-  export function append <H extends HTMLElement> (node: H, ...nodes: Element[]): H
+  export function append <H extends HTMLElement> (node: H,
+    ...nodes: (Element|Parameters<typeof render>)[]): H
 }
