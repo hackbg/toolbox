8fbea1871adfd7f5ed83780b1c51857f74cdb6b7
Adam A <adam@hack.bg>
2024-02-08 19:36:59 +0200

    feat(logs): "icons"

--- logs/logs.mjs
+++ logs/logs.mjs
@@ -35,12 +35,12 @@ export class Console extends defineCallable(function log(...args){
 
   sub = (label, options = {}) => new SubConsole(label, { ...options, parent: this })
   br = () => { this.parent.log(); return this }
-  log   = (...args) => this._print('log',   this._tag(chalk.green,   ' LOG   '), ...args)
-  info  = (...args) => this._print('info',  this._tag(chalk.blue,    ' INFO  '), ...args)
-  warn  = (...args) => this._print('warn',  this._tag(chalk.yellow,  ' WARN  '), ...args)
-  error = (...args) => this._print('error', this._tag(chalk.red,     ' ERROR '), ...args)
-  debug = (...args) => this._print('debug', this._tag(chalk.gray,    ' DEBUG '), ...args)
-  trace = (...args) => this._print('log',   this._tag(chalk.magenta, ' TRACE '), ...args, '\n'+new Error().stack.split('\n').slice(2).join('\n'))
+  log   = (...args) => this._print('log',   this._tag(chalk.green,   ' ✔ '), ...args)
+  info  = (...args) => this._print('info',  this._tag(chalk.blue,    ' i '), ...args)
+  warn  = (...args) => this._print('warn',  this._tag(chalk.yellow,  ' ! '), ...args)
+  error = (...args) => this._print('error', this._tag(chalk.red,     ' × '), ...args)
+  debug = (...args) => this._print('debug', this._tag(chalk.gray,    ' ⋯ '), ...args)
+  trace = (...args) => this._print('log',   this._tag(chalk.magenta, ' ⋮ '), ...args, '\n'+new Error().stack.split('\n').slice(2).join('\n'))
   table = (...args) => {
     this._print('log', this._tag(chalk.white,   'TABLE '));
     this._print('table', ...args)
